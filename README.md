2f20e590b8e70b06d1b1278a3fadce8d86b82392
pbourgeois <pbourgeois@ORDPBOURGEOIS10>
2022-01-03 15:24:07 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Bootstap 5 SASS and PurgeCSS starter kit
+
+Bootstrap 5 Carousel template with Sass and PurgeCss configs.
+
+## Install
+
+```bash
+npm install
+```
+
+## Dev
+To start writing SCSS code and generating CSS automatically :
+
+```bash
+npm run dev
+```
+## Build
+To build a clean css without the unused css selectors
+
+```bash
+npm run build
+```
+
---
+++ purgecss.config.js
@@ -0,0 +1,13 @@
+/* global module */
+/* eslint no-undef: "error" */
+module.exports = {
+    content: ['./dist/index.html'],
+    css: ['./dist/css/main.min.css'],
+    // Add css classes used from javascript to ignore purgecss :
+    safelist: ['carousel-item-start', 'carousel-item-end', 'carousel-item-next', 'carousel-item-prev'],
+    output: './dist/css/',
+    keyframes: true,
+    rejected: true,
+    variables: true
+  }
+  
