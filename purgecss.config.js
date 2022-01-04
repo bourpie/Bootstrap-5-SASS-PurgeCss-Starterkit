612a3e2fbb49640856a9c6896f62b67d63691fa1
pbourgeois <pbourgeois@ORDPBOURGEOIS10>
2022-01-04 13:03:45 -0500

    change output name

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,7 +2,7 @@
 /* eslint no-undef: "error" */
 module.exports = {
     content: ['./dist/index.html'],
-    css: ['./dist/css/main.min.css'],
+    css: ['./dist/css/main.css'],
     // Add css classes used from javascript to ignore purgecss :
     safelist: ['carousel-item-start', 'carousel-item-end', 'carousel-item-next', 'carousel-item-prev'],
     output: './dist/css/',
